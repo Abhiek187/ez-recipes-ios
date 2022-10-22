0fc83c82c8c6ad84eac846f20729d89b9b41cced
Abhishek Chaudhuri <abhishek.chaudhuri@prudential.com>
2022-10-22 17:19:48 -0400

    Create a new iOS project

---
+++ EZ Recipes/EZ Recipes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EZ Recipes
+//
+//  Created by Abhishek Chaudhuri on 10/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EZ Recipes/EZ Recipes/EZ_RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EZ_RecipesApp.swift
+//  EZ Recipes
+//
+//  Created by Abhishek Chaudhuri on 10/22/22.
+//
+
+import SwiftUI
+
+@main
+struct EZ_RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
