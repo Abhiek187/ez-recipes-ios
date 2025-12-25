37ff71bf6cb239578a5770462823eb03f14b04df
Abhiek187 <achaudhuri2011@yahoo.com>
2025-12-25 09:03:10 -0500

    Make the login & signup forms scrollable on small screens

--- EZ Recipes/EZ Recipes/Views/Login/SignUpForm.swift
+++ EZ Recipes/EZ Recipes/Views/Login/SignUpForm.swift
@@ -33,92 +33,96 @@ struct SignUpForm: View {
     @State private var passwordsDoNotMatch = false
     
     var body: some View {
-        VStack(spacing: 16) {
-            HStack {
-                Text(Constants.ProfileView.signUpSubHeader)
-                Button {
-                    router.goBack()
-                } label: {
-                    Text(Constants.ProfileView.signInHeader)
+        ScrollView {
+            VStack(spacing: 16) {
+                HStack {
+                    Text(Constants.ProfileView.signUpSubHeader)
+                    Button {
+                        router.goBack()
+                    } label: {
+                        Text(Constants.ProfileView.signInHeader)
+                    }
+                }
+                .font(.title2)
+                
+                TextField(Constants.ProfileView.emailField, text: $email)
+                    .textContentType(.emailAddress)
+                    .keyboardType(.emailAddress)
+                    .textInputAutocapitalization(.never)
+                    .autocorrectionDisabled()
+                    .textFieldStyle(.roundedBorder)
+                    .focused($focusedField, equals: .email)
+                    .onChange(of: email) {
+                        withAnimation {
+                            emailEmpty = email.isEmpty
+                            emailInvalid = email.wholeMatch(of: Constants.emailRegex) == nil
+                        }
+                    }
+                FormError(on: emailTouched && emailEmpty, message: Constants.ProfileView.fieldRequired("Email"))
+                FormError(on: emailTouched && emailInvalid, message: Constants.ProfileView.emailInvalid)
+                
+                SecureTextField(label: Constants.ProfileView.passwordField, text: $password, isNewPassword: true)
+                    .focused($focusedField, equals: .password)
+                    .onChange(of: password) {
+                        withAnimation {
+                            passwordEmpty = password.isEmpty
+                            passwordTooShort = password.count < Constants.passwordMinLength
+                        }
+                    }
+                    .onChange(of: [password, passwordConfirm]) {
+                        withAnimation {
+                            passwordsDoNotMatch = password != passwordConfirm
+                        }
+                    }
+                FormError(on: passwordTouched && passwordEmpty, message: Constants.ProfileView.fieldRequired("Password"))
+                FormError(on: passwordTouched && passwordTooShort, message: Constants.ProfileView.passwordMinLengthError)
+                
+                SecureTextField(label: Constants.ProfileView.passwordConfirmField, text: $passwordConfirm, isNewPassword: true)
+                    .focused($focusedField, equals: .passwordConfirm)
+                // Supporting text
+                if !passwordsDoNotMatch {
+                    Text(Constants.ProfileView.passwordMinLengthInfo)
+                        .font(.subheadline)
+                }
+                FormError(on: passwordConfirmTouched && passwordsDoNotMatch, message: Constants.ProfileView.passwordMatch)
+                
+                HStack {
+                    Spacer()
+                    ProgressView()
+                        .opacity(viewModel.isLoading ? 1 : 0)
+                    Button {
+                        Task {
+                            await viewModel.createAccount(username: email, password: password)
+                        }
+                    } label: {
+                        Text(Constants.ProfileView.signUpHeader)
+                    }
+                    .font(.title3)
+                    .disabled(emailEmpty || emailInvalid || passwordEmpty || passwordTooShort || passwordsDoNotMatch || viewModel.isLoading)
                 }
             }
-            .font(.title2)
-            
-            TextField(Constants.ProfileView.emailField, text: $email)
-                .textContentType(.emailAddress)
-                .keyboardType(.emailAddress)
-                .textInputAutocapitalization(.never)
-                .autocorrectionDisabled()
-                .textFieldStyle(.roundedBorder)
-                .focused($focusedField, equals: .email)
-                .onChange(of: email) {
-                    withAnimation {
-                        emailEmpty = email.isEmpty
-                        emailInvalid = email.wholeMatch(of: Constants.emailRegex) == nil
+            .padding()
+            .keyboardNavigation(focusedField: $focusedField)
+            .onChange(of: focusedField) {
+                withAnimation {
+                    if focusedField == .email {
+                        emailTouched = true
+                    } else if focusedField == .password {
+                        passwordTouched = true
+                    } else if focusedField == .passwordConfirm {
+                        passwordConfirmTouched = true
                     }
                 }
-            FormError(on: emailTouched && emailEmpty, message: Constants.ProfileView.fieldRequired("Email"))
-            FormError(on: emailTouched && emailInvalid, message: Constants.ProfileView.emailInvalid)
-            
-            SecureTextField(label: Constants.ProfileView.passwordField, text: $password, isNewPassword: true)
-                .focused($focusedField, equals: .password)
-                .onChange(of: password) {
-                    withAnimation {
-                        passwordEmpty = password.isEmpty
-                        passwordTooShort = password.count < Constants.passwordMinLength
-                    }
-                }
-                .onChange(of: [password, passwordConfirm]) {
-                    withAnimation {
-                        passwordsDoNotMatch = password != passwordConfirm
-                    }
-                }
-            FormError(on: passwordTouched && passwordEmpty, message: Constants.ProfileView.fieldRequired("Password"))
-            FormError(on: passwordTouched && passwordTooShort, message: Constants.ProfileView.passwordMinLengthError)
-            
-            SecureTextField(label: Constants.ProfileView.passwordConfirmField, text: $passwordConfirm, isNewPassword: true)
-                .focused($focusedField, equals: .passwordConfirm)
-            // Supporting text
-            if !passwordsDoNotMatch {
-                Text(Constants.ProfileView.passwordMinLengthInfo)
-                    .font(.subheadline)
             }
-            FormError(on: passwordConfirmTouched && passwordsDoNotMatch, message: Constants.ProfileView.passwordMatch)
-            
-            HStack {
-                Spacer()
-                ProgressView()
-                    .opacity(viewModel.isLoading ? 1 : 0)
-                Button {
-                    Task {
-                        await viewModel.createAccount(username: email, password: password)
-                    }
-                } label: {
-                    Text(Constants.ProfileView.signUpHeader)
+            .task(id: viewModel.chef) {
+                if let chef = viewModel.chef, !chef.emailVerified {
+                    await viewModel.sendVerificationEmail()
+                    router.navigate(to: .verifyEmail(email: chef.email))
                 }
-                .font(.title3)
-                .disabled(emailEmpty || emailInvalid || passwordEmpty || passwordTooShort || passwordsDoNotMatch || viewModel.isLoading)
             }
         }
-        .padding()
-        .keyboardNavigation(focusedField: $focusedField)
-        .onChange(of: focusedField) {
-            withAnimation {
-                if focusedField == .email {
-                    emailTouched = true
-                } else if focusedField == .password {
-                    passwordTouched = true
-                } else if focusedField == .passwordConfirm {
-                    passwordConfirmTouched = true
-                }
-            }
-        }
-        .task(id: viewModel.chef) {
-            if let chef = viewModel.chef, !chef.emailVerified {
-                await viewModel.sendVerificationEmail()
-                router.navigate(to: .verifyEmail(email: chef.email))
-            }
-        }
+        .defaultScrollAnchor(.center, for: .alignment)
+        .scrollBounceBehavior(.basedOnSize)
     }
 }
 
