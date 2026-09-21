46920f035aec10b327d2b4134e866e3c60f248fa
Abhiek187 <achaudhuri2011@yahoo.com>
2026-09-20 21:32:16 -0400

    Re-fetch glossary terms in case of timeout

--- EZ Recipes/EZ Recipes/Views/Glossary/GlossaryView.swift
+++ EZ Recipes/EZ Recipes/Views/Glossary/GlossaryView.swift
@@ -8,16 +8,21 @@
 import SwiftUI
 
 struct GlossaryView: View {
-    @State var terms = UserDefaultsManager.getTerms()?.sorted(by: {
-        // Sort all the terms alphabetically for ease of reference
-        $0.word < $1.word
-    })
+    var viewModel: HomeViewModel
+    @State var terms = UserDefaultsManager.getTerms()
+    
+    var sortedTerms: [Term]? {
+        terms?.sorted(by: {
+            // Sort all the terms alphabetically for ease of reference
+            $0.word < $1.word
+        })
+    }
     
     var body: some View {
         NavigationStack {
             Group {
-                if let terms {
-                    List(terms, id: \._id) { term in
+                if let sortedTerms, !sortedTerms.isEmpty {
+                    List(sortedTerms, id: \._id) { term in
                         Text("**\(term.word)** — \(term.definition)")
                     }
                 } else {
@@ -29,20 +34,34 @@ struct GlossaryView: View {
         }
         .onAppear {
             // Update the terms list when switching tabs
-            terms = UserDefaultsManager.getTerms()?.sorted {
-                $0.word < $1.word
-            }
+            terms = UserDefaultsManager.getTerms()
         }
         .task {
-            if let terms {
+            if terms?.isEmpty != false {
+                await viewModel.checkCachedTerms()
+            } else if let terms {
                 try? await SpotlightManager.donateTerms(terms)
             }
         }
     }
 }
 
-#Preview {
+#Preview("No Loading") {
+    let mockRepo = NetworkManagerMock.shared
+    let swiftData = SwiftDataManager.preview
+    let homeViewModel = HomeViewModel(repository: mockRepo, swiftData: swiftData)
+    
     UserDefaultsManager.saveTerms(terms: Constants.Mocks.terms)
     
-    return GlossaryView()
+    return GlossaryView(viewModel: homeViewModel)
+}
+
+#Preview("Loading") {
+    let mockRepo = NetworkManagerMock.shared
+    let swiftData = SwiftDataManager.preview
+    let homeViewModel = HomeViewModel(repository: mockRepo, swiftData: swiftData)
+    
+    UserDefaultsManager.saveTerms(terms: [])
+    
+    return GlossaryView(viewModel: homeViewModel)
 }
